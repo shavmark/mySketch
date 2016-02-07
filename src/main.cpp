937a76d2fb81f95598ac412c4635258d6ddc0414
shavmark <mark@shavlik.us>
2016-02-07 13:38:00 -0600

    add main

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <sapi.h>  
+#include <sphelper.h>  
+#include <Synchapi.h>
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+	
+	ofGLWindowSettings settings;
+	settings.setGLVersion(4, 5); 
+	ofCreateWindow(settings);     // this kicks off the running of my app    
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
+}
