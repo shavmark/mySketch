9fe412955a3c4dbb85826e3986ed2e57f9ba461f
shavmark <mark@shavlik.us>
2016-03-12 17:51:33 -0600

    make private items

--- src/scenes.h
+++ src/scenes.h
@@ -18,18 +18,18 @@ namespace Software2552 {
 		void setup();
 		void update();
 		void draw();
-		void draw2d();
-		void draw3d();
 		void test();
 		void setBackgroundImageName(const string&name) { backgroundImageName = name; }
 	protected:
 		virtual void draw3dFixed() = 0; 
 		virtual void draw3dMoving() = 0;
+	private:
 		vector<Camera>	cameras; // secondary cameras
 		vector<Light>	lights;
 		Material material;//bugbug need to learn this but I expect it pairs with material, just make a vector<pair<>>
 		Director director;
-	private:
+		void draw2d();
+		void draw3d();
 		Camera *camFixed  = nullptr; // set via draw3dStart
 		Camera *camMoving = nullptr; // set via draw3dStart
 
