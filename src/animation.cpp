02cac20008dc66a4b4f95ee0a3d56eceb49ab95a
shavmark <mark@shavlik.us>
2016-03-18 15:38:08 -0500

    add

---
+++ src/animation.cpp
@@ -0,0 +1,126 @@
+#include "2552software.h"
+#include "animation.h"
+
+namespace Software2552 {
+
+
+	Animator::Animator(bool setupIn) {
+		if (setupIn) {
+			resetAnimation();
+		}
+	}
+	//ColorSet Animator::getFirstColor(ColorSet::ColorGroup group) {
+	//	std::vector<ColorSet>::iterator itr = std::find(data.begin(), data.end(), ColorSet(group));
+	//	if (itr != data.end()) {
+	//		++(*itr); // reflect usage
+	//		return *itr;
+	//	}
+	//	return ColorSet(group); // always do something
+	//}
+
+	void Animator::playAnimation() {
+		stopped = paused = false;
+		startAnimation();
+	}
+	// not coded yet
+	void Animator::pauseAnimation() {
+		uint64_t elapsed = ofGetElapsedTimeMillis();
+		// if beyond wait time 
+		// else hold wait time even after pause
+		if (elapsed - (startTime + wait) > 0) {
+			wait = 0; // ignore wait time upon return
+		}
+		paused = true;
+	}
+
+	bool Animator::staticOKToRemovePtr(shared_ptr<Animator> me) {
+		if (me == nullptr) {
+			return false;
+		}
+		return staticOKToRemove(*me);
+	}
+	
+	bool Animator::staticOKToRemove(const Animator& me) {
+		if (me.isExpired()) {
+			return true;
+		}
+		// duration == 0 means never go away, and start == 0 means we have not started yet
+		if (me.duration == 0 || me.startTime == 0) {
+			return false; // no time out ever, or we have not started yet
+		}
+		uint64_t elapsed = ofGetElapsedTimeMillis() - me.startTime;
+		if (me.wait > elapsed) {
+			return false;
+		}
+		if (me.duration > elapsed) {
+			return false;
+		}
+		return true;
+
+	}
+
+	bool Animator::okToDraw() {
+		if (paused || stopped || isExpired()) {
+			return false;
+		}
+		// if still in wait threshold
+		uint64_t t = ofGetElapsedTimeMillis() - startTime;
+		if (t < wait) {
+			return false; // in wait mode, nothing else to do
+		}
+		wait = 0; // skip all future usage of wait once we start
+				  // duration 0 means always draw
+		if (duration == 0) {
+			return true;
+		}
+		if (t < duration) {
+			return true;
+		}
+		else {
+			expired = true;
+			return false;
+		}
+	}
+
+
+
+	void Animator::resetAnimation() {
+		usageCount = 0;
+		startTime = 0;
+		expired = false;
+		paused = false;
+		stopped = false;
+		duration = 0; // infinite bugbug duraiton and wait not full baked in yet
+		wait = 0;
+		rate = 20000;// 20 seconds while developing, but much longer later bugbug set in json
+		w = ofGetWidth();
+		h = ofGetHeight();
+
+	}
+	// return true if a refresh was done
+	bool Animator::refreshAnimation() {
+		if (startTime == 0) {
+			startAnimation();
+			return false;
+		}
+		uint64_t t = ofGetElapsedTimeMillis() - startTime;
+
+		if (t < getWait()) {
+			return false;// waiting to start
+		}
+		wait = 0; // skip all future usage of wait once we start
+				  // check for expired flag 
+		if (duration > 0 && t > duration) {
+			expired = true; // duration of 0 means no expiration
+			return false;
+		}
+		// at this point we can start the time over w/o a wait
+		if (t > refreshRate()) {
+			startTime = ofGetElapsedTimeMillis();
+			return true;
+		}
+		return false;
+	}
+
+
+}
---
+++ src/animation.h
@@ -0,0 +1,93 @@
+#pragma once
+#include "2552software.h"
+#include "ofxAnimatableFloat.h"
+#include "ofxAnimatableOfPoint.h"
+#include "ofxAnimatableOfColor.h"
+// supports animation
+
+namespace Software2552 {
+
+	// basic drawing info, bugbug maybe color set goes here too, not sure yet
+	class DrawingBasics {
+	public:
+		ofPoint pos;
+		int w = 0;
+		int h = 0;
+		bool okToDraw() { return true; }//bugbug move this to the right place
+		string &getLocation() { return locationPath; }
+		uint64_t getWait() { return 0; }//bugbug put in the right place, something from animation
+		uint64_t getLEARNINGDuration() { return 0; }//bugbug put in the right place, something from animation
+	private:
+		string locationPath; // location of item to draw
+
+	};
+
+	// animates colors
+	class ColorAnimation : public ofxAnimatableOfColor {
+	public:
+		void draw();
+	};
+
+	// base class used to draw, drawing done by derived classes
+	class Animatable : public ofxAnimatableFloat, public DrawingBasics {
+	public:
+		virtual void draw();
+		virtual void update(float dt);
+		void set(shared_ptr<ColorAnimation>p);
+
+	private:
+		shared_ptr<ColorAnimation> colorAnimation = nullptr; // optional color
+	};
+
+	// any object that moves, has size or color starts here
+	class Animator : public Animatable {
+	public:
+		Animator(bool setup=true);
+		//bugbug code for a bit then put in read/data in a derived class
+		// defined in model..
+		uint64_t refreshRate() { return rate; /*ms*/ }
+		void setRefreshRate(uint64_t rateIn) { rate = rateIn; };
+		bool refreshAnimation();
+		void startAnimation() { startTime = ofGetElapsedTimeMillis(); }
+		bool isExpired() const { return expired; }
+		bool okToDraw();
+		void operator++ () {++usageCount;}
+		bool operator> (const Animator& rhs) {	return getAnimationUsageCount() > rhs.getAnimationUsageCount();	}
+		int getAnimationUsageCount() const {	return usageCount;	}
+		template<typename T>void removeExpiredPtrToItems(vector<T>&v) {
+			v.erase(std::remove_if(v.begin(), v.end(), Animator::staticOKToRemovePtr), v.end());
+		}
+		template<typename T>void removeExpiredItems(vector<T>&v) {
+			v.erase(std::remove_if(v.begin(), v.end(), Animator::staticOKToRemove), v.end());
+		}
+		virtual void resetAnimation();
+		virtual void pauseAnimation();
+		virtual void playAnimation();
+		virtual void stopAnimation() { stopped = true; }
+		// how long to wait
+		virtual void getTimeBeforeStart(uint64_t& t) {
+			setIfGreater(t, getDuration() + getWait());
+		}
+		uint64_t& getDuration() { return duration; }
+		uint64_t& getWait() { return wait; }
+		void setWait(uint64_t waitIn) { wait = waitIn; }
+		void addWait(uint64_t waitIn) { wait += waitIn; }
+		int w, h; // size
+
+	private:
+		vector<ofPoint> path;
+		static bool staticOKToRemovePtr(shared_ptr<Animator> me);
+		static bool staticOKToRemove(const Animator& me);
+		uint64_t	duration; // life time of object, 0 means forever
+		uint64_t	wait;     // time to wait before drawing
+		uint64_t	startTime;
+		uint64_t	rate;	 // refresh rate
+		bool		expired; // object is expired
+		bool		stopped;
+		bool		paused;
+		int			usageCount; // number of times this animation was used
+	};
+
+
+
+}
