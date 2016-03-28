becac7738a851098f5fd64b222795f7c4856b5aa
shavmark <mark@shavlik.us>
2016-03-27 23:58:21 -0500

    remove 1st element

--- src/scenes.cpp
+++ src/scenes.cpp
@@ -62,6 +62,9 @@ namespace Software2552 {
 		if (backgroundIn) {
 			// should always be first in this vector
 			backgroundIn->readFromScript(data["background"]); // in low memory there will be no back ground
+			if (!getAnimatables().empty) {
+				getAnimatables().pop_front(); // remove current if present, assumes first element is always background
+			}
 			getAnimatables().push_front(backgroundIn);
 		}
 	}
